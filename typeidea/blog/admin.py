f49d9cff2a976334500154ec80cefefb832221a4
Casey Gao <wcgao@mail.ustc.edu.cn>
2022-12-29 19:54:20 +0800

    'admin后台管理界面'

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,119 @@
+# coding=utf-8
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.contrib.admin.models import LogEntry
 
+from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 # Register your models here.
+
+class PostInline(admin.TabularInline):  # StackedInline  样式不同
+    fields = ('title', 'desc')
+    extra = 1  # 控制额外多几个
+    model = Post
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline, ]
+    list_display = ('name', 'owner', 'post_count', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'owner', 'status', 'created_time')
+    fields = ('name', 'status')
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    # 自定义过滤器只展示当前用户分类
+
+    title = '分类过滤器'
+    # 查询时URL参数的名字，比如http://baidu.com?owner_category=1
+    parameter_name = 'owner_category'
+
+    # 返回要展示的内容和查询用的id
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    # 根据URL Query的内容返回列表页数据
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
+    list_display = [
+        'title', 'owner', 'category', 'status', 'created_time', 'operator'
+    ]
+    list_display_links = []
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+
+    exclude = ('owner', )
+
+    """
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+    """
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag', ),
+        })
+    )
+
+
+    # 自定义字段
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+
+    # 引入静态资源
+    # class Media:
+    #     css = {
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     }
+    #     js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
+
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
